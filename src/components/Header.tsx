9fac0c847825c74056aef305889d3b6e403c4937
Koki Sato <s.4115.koki@gmail.com>
2019-03-21 22:00:00 +0900

    Refactor with React Hooks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,41 @@
 import Link from 'next/link'
-import { withRouter, WithRouterProps } from 'next/router'
 import React from 'react'
+import { useToggle } from 'react-use'
 
-interface State {
-  isActive: boolean
-}
+const Header: React.FC = () => {
+  const [open, toggle] = useToggle(false)
+  const toggleNav = () => toggle()
 
-class Header extends React.Component<WithRouterProps, State> {
-  constructor(props: WithRouterProps) {
-    super(props)
-
-    this.state = {
-      isActive: false
-    }
-
-    this.toggleNav = this.toggleNav.bind(this)
-  }
-
-  public render() {
-    return (
-      <nav className="navbar is-fixed-top">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link href="/">
-              <a className="is-size-5">Portfolio</a>
-            </Link>
-            <a className="navbar-burger burger" onClick={this.toggleNav}>
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-            </a>
-          </div>
-          <div className={this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons">
-                  <Link href="/about">
-                    <button className="button is-white">About</button>
-                  </Link>
-                  <Link href="/contact">
-                    <button className="button is-white">Contact</button>
-                  </Link>
-                </div>
+  return (
+    <nav className="navbar is-fixed-top">
+      <div className="container">
+        <div className="navbar-brand">
+          <Link href="/">
+            <a className="is-size-5">Portfolio</a>
+          </Link>
+          <a className="navbar-burger burger" onClick={toggleNav}>
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+        <div className={open ? 'navbar-menu is-active' : 'navbar-menu'}>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <Link href="/about">
+                  <button className="button is-white">About</button>
+                </Link>
+                <Link href="/contact">
+                  <button className="button is-white">Contact</button>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-      </nav>
-    )
-  }
-
-  private toggleNav() {
-    this.setState((prevState) => ({
-      isActive: !prevState.isActive
-    }))
-  }
+      </div>
+    </nav>
+  )
 }
 
-export default withRouter(Header)
+export default Header
