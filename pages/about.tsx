00e33e7ee82472aeedffccb91be7a56e905fbc5a
koki-sato <admin@koki-sato.com>
2019-03-28 22:00:00 +0900

    Implement about page

---
+++ pages/about.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const About: React.FC = () => (
+  <div className="p-about">
+    <h1 className="title">Koki Sato</h1>
+    <div className="panel">
+      <h2 className="subtitle">About</h2>
+      <ul>
+        <li>Master's student in Computer Science major</li>
+        <li>Web Engineer</li>
+        <li>CTF Player (Team m1z0r3)</li>
+      </ul>
+    </div>
+    <div className="panel">
+      <h2 className="subtitle">Career History</h2>
+      <ul>
+        <li>
+          2019/04 - Present : <b>NTT Communications Corporation</b> -- <i>ICT Engineer</i>
+        </li>
+        <li>
+          2016/10 - 2019/02 : <b>Increments inc.</b> -- <i>Engineer part-time job</i>
+        </li>
+        <li>
+          2018/03 - 2018/03 (4 weeks) : <b>LINE Corporation</b> -- <i>Engineer internship</i>
+        </li>
+        <li>
+          2017/08 - 2018/01 : <b>Wantedly inc.</b> -- <i>Engineer internship</i>
+        </li>
+      </ul>
+    </div>
+    <div className="panel">
+      <h2 className="subtitle">Education</h2>
+      <ul>
+        <li>
+          2017/04 - 2019/03 :{' '}
+          <b>Waseda University Graduate School, Fundamental Science and Engineering</b>
+        </li>
+        <li>
+          2013/04 - 2017/03 : <b>Waseda University, Fundamental Science and Engineering</b>
+        </li>
+      </ul>
+    </div>
+    <div className="panel">
+      <h2 className="subtitle">Certification</h2>
+      <ul>
+        <li>Fundamental Information Technology Engineer</li>
+        <li>Applied Information Technology Engineer</li>
+        <li>Network Specialist</li>
+      </ul>
+    </div>
+    <div className="panel">
+      <h2 className="subtitle">Links</h2>
+      <ul>
+        <li>
+          <a href="https://twitter.com/koki310dm">Twitter</a>
+        </li>
+        <li>
+          <a href="https://github.com/koki-sato">GitHub</a>
+        </li>
+        <li>
+          <a href="https://qiita.com/koki-sato">Qiita</a>
+        </li>
+        <li>
+          <a href="https://www.wantedly.com/users/18666745">Wantedly</a>
+        </li>
+        <li>
+          <a href="http://k-sato.hatenablog.com">Hatena Blog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default About
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Footer: React.FC = () => <footer>Copyright &copy; 2019 Koki Sato All Rights Reserved.</footer>
+const Footer: React.FC = () => (
+  <React.Fragment>
+    <div className="footer-line">
+      <div className="footer-line_green" />
+      <div className="footer-line_black" />
+    </div>
+    <footer>Copyright &copy; 2019 Koki Sato All Rights Reserved.</footer>
+  </React.Fragment>
+)
 
 export default Footer
