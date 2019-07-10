1fed44943b82d6eff353f99395c7ca69a8eb67c3
koki-sato <admin@koki-sato.com>
2019-07-11 00:00:00 +0900

    Migrate to Next.js 9

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript(withSass(withCSS()))
+module.exports = withSass(withCSS())
